8c031100296fc478316ccdad89a78ae5d9959c8a
Alexander Ar-Staelin <alexander@Alexanders-MacBook-Pro.local>
2015-12-24 16:23:54 +0200

    added spHashTable.h

---
+++ spHashTable.h
@@ -0,0 +1,26 @@
+/*
+ * spHashTable.h
+ *
+ *  Created on: Dec 24, 2015
+ *      Author: alexander
+ */
+
+#ifndef SPHASHTABLE_H_
+#define SPHASHTABLE_H_
+
+//define struct SH_HASH in spHashTable.c
+#define struct SP_HASH* SP_Hashtable;
+
+//spHashCreate
+
+//spHashDestroy
+
+//spHashGetSize
+
+//spHashInsert
+
+//spHashGetValue
+
+//spHashDelete
+
+#endif /* SPHASHTABLE_H_ */
