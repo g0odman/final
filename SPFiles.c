45f145da505ce69a350a758ea6ab3ab7d5309f05
Uri Goodman <uri.goodman@gmail.com>
2016-01-06 17:36:00 +0200

    Command line checking

--- SPFiles.c
+++ SPFiles.c
@@ -4,10 +4,34 @@
 #include "SPHashTable.h"
 #include "SP_Tree.h"
 #define MAX_LINE_SIZE 200
+
+bool isValidCommandLineArgumentsList(int argc, char** argv){
+    if (argc % 2 == 0 || argc > 5){
+        printf("Invalid command line arguments, use [-v filename1] [-o filename2]");
+        return false;
+    }
+    for(int i = 1;i < argc; i+=2){
+        if((strcmp(argv[i],"-v") != 0&&(strcmp(argv[i],"-o") != 0))){
+            printf("Invalid command line arguments, use [-v filename1] [-o filename2]");
+            return false;
+        }
+    }
+    if(argc == 5){
+        if(strcmp(argv[1] ,argv[3])==0){
+            printf("Invalid command line arguments, use [-v filename1] [-o filename2]");
+           return false;
+        }
+        if(strcmp(argv[2],argv[4]) ==0){
+            printf("Files must be different");
+            return false;
+        }
+    }
+    return true;
+}
 void getVariables(int argc, char **argv, SP_HASH *variables){
     for(int i =1; i < argc; i++){
         if(argv[i][0] == '-' && argv[i][1] == 'v'){
-            FILE *fp = fopen(argv[i+1],"r");
+            FILE *fp = fopen(argv[i+1],"r");//Check if error
             char * line = (char *) malloc(MAX_LINE_SIZE+1);
             while(fgets(line,MAX_LINE_SIZE,fp) != NULL){
                 char *variable = strtok(line,"\r\t\n ");
@@ -26,7 +50,7 @@ bool toPrint(int argc, char **argv, char **filename){
     for(int i =1; i < argc; i++){
         if(argv[i][0] == '-' && argv[i][1] == 'o'){
             *filename = malloc(strlen(argv[i+1]));
-            strcpy(*filename,argv[i+1]);
+            strcpy(*filename,argv[i+1]);//error
             return true;
         }
     }
